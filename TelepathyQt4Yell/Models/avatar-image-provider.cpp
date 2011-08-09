0b503578200500900135518a91cd428e2e45e54f
Alvaro Soliverez <alvaro.soliverez@collabora.co.uk>
2011-08-09 11:34:38 -0700

    Added API docs to avatar image provider class

--- TelepathyQt4Yell/Models/avatar-image-provider.cpp
+++ TelepathyQt4Yell/Models/avatar-image-provider.cpp
@@ -36,6 +36,19 @@ struct TELEPATHY_QT4_YELL_MODELS_NO_EXPORT AvatarImageProvider::Private
     Tp::AccountManagerPtr mAM;
 };
 
+/**
+ * \class AvatarImageProvider
+ * \ingroup models
+ * \headerfile TelepathyQt4Yell/avatar-image-provider.h <TelepathyQt4Yell/AvatarImageProvider>
+ *
+ * \brief This This class provides the avatars for Telepathy accounts and contacts in a suitable format for QML applications
+ *
+ */
+
+/**
+  * Construct an AvatarImageProvider object
+  * \param am A valid AccountManager pointer
+  */
 AvatarImageProvider::AvatarImageProvider(const Tp::AccountManagerPtr &am)
     : QDeclarativeImageProvider(Image),
       mPriv(new Private(am))
@@ -47,16 +60,30 @@ AvatarImageProvider::~AvatarImageProvider()
     delete mPriv;
 }
 
+/**
+  * Returns a URL for the avatar of a given account
+  */
 QString AvatarImageProvider::urlFor(const Tp::AccountPtr &account)
 {
     return QString::fromLatin1("image://avatars/") + account->uniqueIdentifier();
 }
 
+/**
+  * Create an instance and register it to serve as a provider in QDeclarative (QML)
+  * \param engine The application QtDeclarative engine
+  * \param am A valid AccountManager pointer
+  */
 void AvatarImageProvider::registerProvider(QDeclarativeEngine *engine, const Tp::AccountManagerPtr &am)
 {
     engine->addImageProvider(QString::fromLatin1("avatars"), new AvatarImageProvider(am));
 }
 
+/**
+  * Returns the avatar image for a given account id
+  * \param id An account id
+  * \param size If a valid pointer, it will be set to the image size
+  * \param requestedSize This is not implemented
+  */
 QImage AvatarImageProvider::requestImage(const QString &id, QSize *size, const QSize &requestedSize)
 {
     QString path = QString::fromLatin1(TELEPATHY_ACCOUNT_OBJECT_PATH_BASE "/") + id;
